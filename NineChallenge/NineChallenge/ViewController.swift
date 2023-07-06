841eb9f29b6c62fed147a464086fd1df9929a592
Xavier Zhang <overeggland@gmail.com>
2023-07-06 12:37:19 +1000

    Chore: Project Initialization

---
+++ NineChallenge/NineChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NineChallenge
+//
+//  Created by Xavier Zhang on 6/7/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
